3643b705fddfb59d10f538a3423553a22976e530
Dom Owens <do16317@bristol.ac.uk>
2019-10-08 16:00:39 +0100

    init

---
+++ Packages/Example/R/hello.R
@@ -0,0 +1,19 @@
+# An example package for Statistical Computing 1
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+sum <- function(x) {
+  s <-0
+  for (i in 1:length(x)) {
+    s <- s + x[i]
+  }
+  s
+}
